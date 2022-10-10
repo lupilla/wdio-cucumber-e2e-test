d91a8ad6c14c567fb85fefe8b34606d710900621
lupilla <lupilla77@gmail.com>
2022-10-10 15:16:26 +0200

    feat: add demo feature and step definitions

---
+++ test/features/step-definitions/demo.ts
@@ -0,0 +1,26 @@
+import { Given, When, Then } from "@cucumber/cucumber";
+import { expect } from "chai";
+
+Given(/^Google page is opened$/, async function() {
+  await browser.url("https://www.google.com");
+  await browser.pause(1000);
+  let element = await $(`#L2AGLb`);
+  await element.click();
+  await browser.pause(1000);
+});
+
+When(/^Search with (.*)$/, async function(searchItem) {
+  let element = await $(`[name="q"]`);
+  await element.setValue(searchItem);
+  await browser.keys("Enter");
+});
+
+Then(/^Click on the first search result$/, async function() {
+  let element = await $(`<h3>`);
+  element.click();
+});
+
+Then(/^URL should match (.*)$/, async function(expectedURL) {
+  let browserURL = await browser.getUrl()
+  expect(browserURL).to.equal(expectedURL);
+});
