f308d1b3b44f3f4e7fc83c5ef4c0578448db1ce4
lupilla <lupilla77@gmail.com>
2022-10-12 14:41:02 +0200

    feat: add waitUntill example
    waitUntill example

--- test/features/step-definitions/BasicScroll.ts
+++ test/features/step-definitions/BasicScroll.ts
@@ -13,9 +13,37 @@ When(/^Perform basic scroll interactions$/, async function() {
    * Methods used:
       1. scrollIntoView()
   */
-  await browser.pause(1000);
+  await browser.pause(2000);
   await (await $(`#page-footer`)).scrollIntoView();
   //If you want to get the element at the bottom, use false flag:
   //await (await $(`#page-footer`)).scrollIntoView(false);
-  await browser.pause(1000);
+  await browser.pause(2000);
+  
+  await (await $(`<h1>`)).scrollIntoView();
+  await browser.pause(2000);
+  
+  //Advanced scrolling:
+  //Scroll to Bottom:
+  await browser.execute(() => {
+    window.scrollBy(0, window.innerHeight);
+  })
+  await browser.pause(2000);
+  
+  //Scroll to Top:
+  await browser.execute(() => {
+    window.scrollBy(0, -window.innerHeight);
+  })
+  await browser.pause(2000);
+  
+  //scrollTo: To bottom
+  await browser.execute(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  })
+  await browser.pause(2000);
+  
+  //scrollTo: To top
+  await browser.execute(() => {
+    window.scrollTo(0, document.body.scrollTop);
+  })
+  await browser.pause(2000);
 });
--- test/features/step-definitions/Search.ts
+++ test/features/step-definitions/Search.ts
@@ -7,12 +7,15 @@ Given(/^Google page with a search field is opened$/, async function() {
   let element = await $(`#L2AGLb`);
   await element.click();
   await browser.pause(1000);
+  //console.log(`After opening browser`);
+  //console.log(`Browser: ${JSON.stringify(browser)}`);
 });
 
 When(/^Search with (.*)$/, async function(searchItem) {
   let element = await $(`[name="q"]`);
   await element.setValue(searchItem);
   await browser.keys("Enter");
+  //console.log(`Element: ${JSON.stringify(element)}`);
 });
 
 Then(/^Click on the first search result$/, async function() {
@@ -21,6 +24,10 @@ Then(/^Click on the first search result$/, async function() {
 });
 
 Then(/^URL should match (.*)$/, async function(expectedURL) {
-  let browserURL = await browser.getUrl()
+  await browser.waitUntil(async function() {
+    //return await $('selector').isDisplayed....
+    return await browser.getTitle () === "WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO"
+  }, {timeout: 20000, interval: 500, timeoutMsg: `Fail loading WDIO web page ${await browser.getTitle()}`});
+  let browserURL = await browser.getUrl();
   expect(browserURL).to.equal(expectedURL);
 });
